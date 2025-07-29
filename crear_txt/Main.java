beffa007fcbd8a1747cdb0fbb1bf34e9acfe2fd4
CesarMontoyag1 <159492748+CesarMontoyag1@users.noreply.github.com>
2025-07-28 20:54:52 -0500

    Add files via upload

---
+++ crear_txt/Main.java
@@ -0,0 +1,19 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Ingrese el nombre a guardar: ");
+        Scanner scanner = new Scanner(System.in);
+        String nombre = scanner.nextLine();
+
+        try (FileWriter writer = new FileWriter("archivo.txt")) {
+            writer.write(nombre);
+            System.out.println("Nombre guardado en archivo.txt");
+        } catch (IOException e) {
+            System.out.println("Error al guardar el nombre: " + e.getMessage());
+        }
+
+
+    }
+}
